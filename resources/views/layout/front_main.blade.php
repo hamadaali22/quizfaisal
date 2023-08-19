2558a884ea2d213d74b57641b4b5b9a31278b9b4
hamada <mariam.hamada.2020@gmail.com>
2023-08-19 09:27:03 +0300

    m2

--- resources/views/layout/front_main.blade.php
+++ resources/views/layout/front_main.blade.php
@@ -3,6 +3,18 @@
 <head>
   @include('layout.front_head')
   <!-- <link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}"> -->
+  <style>
+     body {
+     -webkit-user-select: none;
+     -moz-user-select: none;
+     -ms-user-select: none;
+     user-select: none;
+     }
+  </style>
+
+
+
+
 </head>
 <body>
    <div id="app">
@@ -20,5 +32,11 @@
     <script src="{{asset('front/main.js')}}"></script>
 
     <script src="{{asset('js/app.js')}}"></script>
+
+    <script type="text/javascript">
+       $(document).bind("contextmenu",function(e){
+       return false;
+       });
+    </script>
 </body>
 </html>

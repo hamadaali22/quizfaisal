19a5c32e040e700b289309d1c222e6a848ae2b7d
Hamada <hamada@Hamadas-MBP.local>
2024-04-05 16:55:04 +0200

    47

--- resources/js/components/PlacementTest.vue
+++ resources/js/components/PlacementTest.vue
@@ -2,16 +2,42 @@
     <div>
         <section class="row home-main-section p-5 container-fluid">
             <div class="col-lg m-auto pl-4">
-                <p>Viele Prüfungsteilnehmer sprechen oft von Goethe Deutsch oder Telc Deutsch. Sie meinen berechtigt damit, dass die Prüfungen beim Goethe-Institut oder bei Telc einer speziellen Vorbereitung bedürfen.</p>
-                <p>Die Mehrheit von ihnen beschweren sich nicht selten, aber zurecht, dass sie nicht genügend Material zur Vorbereitung auf die Prüfungen besitzen. Außerhalb Deutschland sind Materialien eine seltene Ware. Wir wissen genauso auch,  Deutsch Tests online zu finden, ist schwierig. Das wollten wir ändern und den Lernenden unter die Arme greifen.</p>
+                <p v-html="contactInfo.placement_desc"></p>
                 <router-link :to="'/placement-questions'" class="a-link">
-                    <div  class="level  w-50 text-center text-light mt-1 pt-2 pb-2">
+                    <div class="level  w-50 text-center text-light mt-1 pt-2 pb-2">
                         Start Test
                     </div>
                 </router-link>
             </div>
-            
-            
+
+
         </section>
     </div>
 </template>
+<script>
+
+export default {
+    data() {
+        return {
+
+        }
+
+    },
+    computed: {
+
+        contactInfo() {
+            return this.$store.state.contactInfo
+        }
+    },
+
+    mounted() {
+        this.$store.dispatch('getContactinfo');
+    },
+
+    methods: {
+
+    },
+
+
+}
+</script>

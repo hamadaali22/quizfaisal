64dcc21129a463bd600a1e32a60d9f41c4dcf73b
Hamada <hamada@Hamadas-MBP.local>
2024-04-12 17:09:15 +0200

    50

--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -1,13 +1,13 @@
-
 <template>
-    <div v-bind:class="[this.$route.path == '/noresult' || this.$route.path=='/placement-test'? 'fixed-bottom' : '']">
+    <div
+        v-bind:class="[this.$route.path == '/noresult' || this.$route.path == '/placement-test' || this.$route.path == '/placement-result' ? 'fixed-bottom' : '']">
         <br><br><br>
         <!-- <footer class="back navbar " v-bind:class="[this.$route.path == '/noresult' || this.$route.path =='/goethe-user-exam' ? 'fixed-bottom' : '']"> -->
 
-        <footer class="back navbar " >
-                <!-- <div class=" "> -->
-                    
-                    <!-- <div class="row col-md-12" style="    padding-right: 0px;">
+        <footer class="back navbar ">
+            <!-- <div class=" "> -->
+
+            <!-- <div class="row col-md-12" style="    padding-right: 0px;">
                         <div class="col-md-2" >
                             <div class="widget widget-about">
                                 
@@ -50,7 +50,7 @@
                         </div>
                         
                     </div> -->
-                    <!-- <div class="row col-md-12" style="padding-right: 0px;">
+            <!-- <div class="row col-md-12" style="padding-right: 0px;">
                         <div class="col-md-2" >
                             
                         </div>
@@ -83,65 +83,82 @@
                             
                         </div>
                     </div> -->
-                    <div class="row col-md-12" >
-                        <div class="col-sm-6 col-md-3 ">
-                            <div class="widget widget-about">
-                                
-                                <div class="links text-center">
-                                    <img :src="contactInfo.logo" class="footer-logo" alt="“deutschtests Logo" width="100" height="100"><br>
-                                   
-                                </div>
+            <div class="row col-md-12">
+                <div class="col-sm-6 col-md-3 ">
+                    <div class="widget widget-about">
+
+                        <div class="links text-center">
+                            <img :src="contactInfo.logo" class="footer-logo" alt="“deutschtests Logo" width="100"
+                                height="100"><br>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="row col-md-9 col-xs-12 col-sm-6">
+                    <div class="d-flex flex-column flex-md-row">
+                        <div class="d-flex flex-row ">
+                            <div class="p-2">
+                                <h6 class="widget-title"><a href="/goethe-tests">Goethe Tests</a></h6>
                             </div>
                         </div>
-                        <div class="row col-md-9 col-xs-12 col-sm-6" >
-                            <div class="d-flex flex-column flex-md-row">
-                                <div class="d-flex flex-row ">
-                                    <div class="p-2"><h6 class="widget-title" ><a href="/goethe-tests">Goethe Tests</a></h6></div>
-                                </div>
-                                <div class="d-flex flex-row">
-                                    <div class="p-2" style="font-size: 14px;"><a href="/goethe-tests/goethe-a1-zertifikat" >Goethe A1 Zertifikat</a></div>
-                                    <div class="p-1"></div>
-                                </div>
-                                <div class="d-flex flex-row">
-                                    <div class="p-2" style="font-size: 14px;"><a href="/goethe-tests/goethe-zertifikat-a2">Goethe A2 Zertifikat</a></div>
-                                    <div class="p-1"></div>
-                                </div>
-                                <div class="d-flex flex-row">
-                                    <div class="p-2" style="font-size: 14px;"><a href="/goethe-tests/goethe-b1-modelltest">Goethe B1 Modelltest</a></div>
-                                    
-                                </div>
-                                <div class="d-flex flex-row">
-                                    <div class="p-2" style="font-size: 14px;"><a href="/goethe-tests/goethe-b2-modelltest">Goethe B2 Modelltest</a></div>
-                                </div>
+                        <div class="d-flex flex-row">
+                            <div class="p-2" style="font-size: 14px;"><a
+                                    href="/goethe-tests/goethe-a1-zertifikat">Goethe A1 Zertifikat</a></div>
+                            <div class="p-1"></div>
+                        </div>
+                        <div class="d-flex flex-row">
+                            <div class="p-2" style="font-size: 14px;"><a
+                                    href="/goethe-tests/goethe-zertifikat-a2">Goethe A2 Zertifikat</a></div>
+                            <div class="p-1"></div>
+                        </div>
+                        <div class="d-flex flex-row">
+                            <div class="p-2" style="font-size: 14px;"><a
+                                    href="/goethe-tests/goethe-b1-modelltest">Goethe B1 Modelltest</a></div>
+
+                        </div>
+                        <div class="d-flex flex-row">
+                            <div class="p-2" style="font-size: 14px;"><a
+                                    href="/goethe-tests/goethe-b2-modelltest">Goethe B2 Modelltest</a></div>
+                        </div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row">
+
+                        <div class="d-flex flex-row ">
+                            <div class="p-2"></div>
+                            <div class="p-2">
+                                <h6 class="widget-title"><a href="/telc-tests"> Telc Tests</a></h6>
                             </div>
-                            <div class="d-flex flex-column flex-md-row">
-                                
-                                <div class="d-flex flex-row ">
-                                    <div class="p-2"></div>
-                                    <div class="p-2"><h6 class="widget-title" ><a href="/telc-tests"> Telc Tests</a></h6></div>
-                                </div>
-                                <div class="d-flex flex-row">
-                                    <div class="p-2"></div><div class="p-1"></div>
-                                    <div class="p-2" style="font-size: 14px;"><a href="/telc-tests/telc-a2-modelltest">Telc A1 Modelltest</a></div>
-                                    
-                                </div>
-                               
-                                <div class="d-flex flex-row">
-                                    <div class="p-2"></div><div class="p-1"></div>
-                                    <div class="p-2" style="font-size: 14px;"><a href="/telc-tests/telc-a2-modelltest">Telc A2 Modelltest </a></div>
-                                    
-                                </div>
-                                <div class="d-flex flex-row">
-                                    <div class="p-2"></div><div class="p-1"></div>
-                                    <div class="p-2" style="font-size: 14px;"><a href="/telc-tests/telc-b1-modelltest">Telc B1 Modelltest</a></div>
-                                   
-                                </div>
-                                <div class="d-flex flex-row">
-                                    <div class="p-2"></div><div class="p-1"></div>
-                                    <div class="p-2" style="font-size: 14px;"><a href="/telc-tests/telc-b2-modelltest"> Telc B2 Modelltest </a></div>
-                                </div>
-                            </div>
-                            <!-- <div class="d-flex flex-column flex-md-row">
+                        </div>
+                        <div class="d-flex flex-row">
+                            <div class="p-2"></div>
+                            <div class="p-1"></div>
+                            <div class="p-2" style="font-size: 14px;"><a href="/telc-tests/telc-a2-modelltest">Telc A1
+                                    Modelltest</a></div>
+
+                        </div>
+
+                        <div class="d-flex flex-row">
+                            <div class="p-2"></div>
+                            <div class="p-1"></div>
+                            <div class="p-2" style="font-size: 14px;"><a href="/telc-tests/telc-a2-modelltest">Telc A2
+                                    Modelltest </a></div>
+
+                        </div>
+                        <div class="d-flex flex-row">
+                            <div class="p-2"></div>
+                            <div class="p-1"></div>
+                            <div class="p-2" style="font-size: 14px;"><a href="/telc-tests/telc-b1-modelltest">Telc B1
+                                    Modelltest</a></div>
+
+                        </div>
+                        <div class="d-flex flex-row">
+                            <div class="p-2"></div>
+                            <div class="p-1"></div>
+                            <div class="p-2" style="font-size: 14px;"><a href="/telc-tests/telc-b2-modelltest"> Telc B2
+                                    Modelltest </a></div>
+                        </div>
+                    </div>
+                    <!-- <div class="d-flex flex-column flex-md-row">
                                 <div class="d-flex flex-row ">
                                     <div class="p-2"><h6 class="widget-title" ><a href="/goethe-tests">Goethe Tests</a></h6></div>
                                 </div>
@@ -187,11 +204,11 @@
                                     <div class="p-2" style="font-size: 14px;"><a href="/telc-tests/telc-b2-modelltest">Telc B2 Modelltest </a></div>
                                 </div>
                             </div> -->
-                        </div>
+                </div>
 
 
 
-                            <!-- <div class="d-flex flex-row">
+                <!-- <div class="d-flex flex-row">
                                 <div class="p-2"><h5 class="widget-title"><a href="/goethe-tests">Goethe Tests</a></h5></div>
                                 <div class="p-2"><a href="/goethe-tests/goethe-a1-zertifikat" >Goethe A1 Zertifikat</a></div>
                                 <div class="p-2"><a href="#">|</a></div>
@@ -213,7 +230,7 @@
                                 <div class="p-2"><a href="/telc-tests/telc-b1-modelltest">Telc B1 Modelltest</a></div>
                             </div> -->
 
-                            <!-- <div class="d-flex  flex-column">
+                <!-- <div class="d-flex  flex-column">
                                 <div class="p-2"><h5 class="widget-title"><a href="/goethe-tests">Goethe Tests</a></h5></div>
                             </div>
                             <div class="d-flex flex-column">
@@ -275,10 +292,10 @@
                             <div class="d-flex flex-column">
                                 <div class="p-2"><a href="/telc-tests/telc-b2-modelltest">Telc B2 Modelltest </a></div>
                             </div> -->
-                        <!-- </div> -->
-                        
-                    </div>
-                    <!-- <div class=" row">
+                <!-- </div> -->
+
+            </div>
+            <!-- <div class=" row">
                                 <div class="p-2 row col-md-2" >
                                     
                                 </div>
@@ -293,25 +310,30 @@
                                     
                                 </div>
                             </div> -->
-                <!-- </div> -->
-                
+            <!-- </div> -->
+
         </footer>
         <!-- padding-top: 9px -->
-        <div  style="background-color: #fff ;">
-                        <div class="container" >
-                            <div class=" row">
-                                <div class="row col-md-10 ">
-                                    <p style="    margin-left: 5px;margin-top: 10px; margin-bottom:0px;"> Copyright © Deutschtests - All Rights Reserved</p>
-                                </div>
-                                <div class="links text-center ">
-                                    <a href="#"><i class="fa-brands fa-facebook p-2 mr-2" style="color:#3e83b3;font-size: 25px !important; "></i></a>
-                                    <a href="#"><i class="fa-brands fa-instagram p-2 mr-2" style="color:#3e83b3;font-size: 25px !important; "></i></a>
-                                    <a href="#"><i class="fa-brands fa-twitter p-2 mr-2" style="color:#3e83b3;font-size: 25px !important;"></i></a>
-                                    <a href="#"><i class="fa-brands fa-github p-2 mr-2" style="color:#3e83b3;font-size: 25px !important;"></i></a>               
-                                </div>
-                            </div>
-                        </div>
+        <div style="background-color: #fff ;">
+            <div class="container">
+                <div class=" row">
+                    <div class="row col-md-10 ">
+                        <p style="    margin-left: 5px;margin-top: 10px; margin-bottom:0px;"> Copyright © Deutschtests -
+                            All Rights Reserved</p>
                     </div>
+                    <div class="links text-center ">
+                        <a href="#"><i class="fa-brands fa-facebook p-2 mr-2"
+                                style="color:#3e83b3;font-size: 25px !important; "></i></a>
+                        <a href="#"><i class="fa-brands fa-instagram p-2 mr-2"
+                                style="color:#3e83b3;font-size: 25px !important; "></i></a>
+                        <a href="#"><i class="fa-brands fa-twitter p-2 mr-2"
+                                style="color:#3e83b3;font-size: 25px !important;"></i></a>
+                        <a href="#"><i class="fa-brands fa-github p-2 mr-2"
+                                style="color:#3e83b3;font-size: 25px !important;"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
         <!-- <footer class="back navbar " v-bind:class="[this.$route.path == '/noresult' || this.$route.path =='/goethe-user-exam' ? 'fixed-bottom' : '']">
                 <div class="container ">
                     <div class="row col-md-12" style="font-size: 15px;">
@@ -365,28 +387,28 @@
                 </div>
         </footer> -->
     </div>
-  </template>
-  
-  
+</template>
+
+
 <script>
 export default {
-  mounted(){
-    this.$store.dispatch('getContactinfo');
-  },
- 
-  computed:{
-    isLogged(){
-      return this.$store.getters.isLogged
+    mounted() {
+        this.$store.dispatch('getContactinfo');
     },
-    contactInfo(){
-      return this.$store.state.contactInfo
+
+    computed: {
+        isLogged() {
+            return this.$store.getters.isLogged
+        },
+        contactInfo() {
+            return this.$store.state.contactInfo
+        }
+    },
+    methods: {
+        logout() {
+            this.$store.commit('logout')
+        }
     }
-  },
-  methods:{
-    logout(){
-       this.$store.commit('logout')
-    }
-  }
 
 
 }
--- resources/js/components/PlacementResult.vue
+++ resources/js/components/PlacementResult.vue
@@ -1,62 +1,73 @@
 <template>
-    <div>
-      <section class="row home-main-section p-5 container-fluid">
-        <div class="col-lg m-auto pl-4 text-center">
-            <p>level: {{ this.levelName }}</p>
-            <p>type: {{ this.type }}</p>
-            <p>correct answer: {{ this.correct }}</p>
-        </div>
-        <!-- <h2 class="text-center ">Sie müssen sich registrieren, um Ihr Ergebnis zu erhalten und die Musterlösung zu sehen.</h2> -->
+  <div>
+    <section class="row home-main-section p-5 container-fluid" v-if="this.result">
+      <div class=" col-lg m-auto pl-4 text-center">
+        <p>level: {{ this.levelName }}</p>
+        <p>type: {{ this.type }}</p>
+        <p>correct answer: {{ this.correct }}</p>
+      </div>
+      <!-- <h2 class="text-center ">Sie müssen sich registrieren, um Ihr Ergebnis zu erhalten und die Musterlösung zu sehen.</h2> -->
 
-        <br><br><br>
-       </section>
-       
-       <!-- <footer class="back navbar " v-bind:class="[this.$route.path == '/noresult' ? 'fixed-bottom' : '']">
+      <br><br><br>
+    </section>
+    <section class="row home-main-section p-5 container-fluid">
+      <div class="col-lg m-auto pl-4">
+
+        <router-link :to="'/placement-questions'" class="a-link">
+          <div class="level  w-50 text-center text-light mt-1 pt-2 pb-2">
+            Start Test
+          </div>
+        </router-link>
+      </div>
+
+
+    </section>
+    <!-- <footer class="back navbar " v-bind:class="[this.$route.path == '/noresult' ? 'fixed-bottom' : '']">
         <p>dfvdf</p>
         </footer> -->
-    </div>
- </template>
- 
- <script>
- 
- export default {
+  </div>
+</template>
+
+<script>
+
+export default {
   data() {
     return {
-      // itemId: null,
-      levelName:null,
-      type:null,
-      correct:null
+      result: null,
+      levelName: null,
+      type: null,
+      correct: null
     };
   },
-  created(){
+  created() {
     this.getUserdata();
   },
   mounted() {
     // this.itemId = this.$route.params.id;
   },
-  methods:{
-  getUserdata(){
-        const headers = {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer '+this.$store.state.userToken.token
-          };
-          axios.get('https://deutschtests.com/api/quizes-result',{headers})
-          .then(res => {
-              console.log(res.data);
-              this.levelName = res.data.data.levelName;
-              this.type=res.data.data.type;
-              this.correct=res.data.data.correct;
-              
-            })
-            .then(err => console.log(err))
+  methods: {
+    getUserdata() {
+      const headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + this.$store.state.userToken.token
+      };
+      axios.get('https://deutschtests.com/api/quizes-result', { headers })
+        .then(res => {
+          this.result = res.data.data;
+          if (res.data.data) {
+            console.log(res.data);
+
+            this.levelName = res.data.data.levelName;
+            this.type = res.data.data.type;
+            this.correct = res.data.data.correct;
+          }
+        })
+        .then(err => console.log(err))
+
     }
   }
-  
 
- 
- }
- </script>
- 
- 
- 
- 
+
+
+}
+</script>

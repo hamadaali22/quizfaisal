09f258b2375942f4989cc412a9563c3ebbe5cdb9
Hamada <hamada@Hamadas-MBP.local>
2025-01-17 10:24:39 +0300

    fix register

--- public/js/app.js
+++ public/js/app.js
@@ -73293,7 +73293,7 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("div", [_c("language-switcher"), _vm._v(" "), _c("router-view")], 1)
+  return _c("div", [_c("router-view")], 1)
 }
 var staticRenderFns = []
 render._withStripped = true

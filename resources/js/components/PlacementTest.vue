e35d08fb69dac28279cddf2b909afcbe38f13739
Hamada <hamada@Hamadas-MBP.local>
2025-11-15 13:55:08 +0300

    add exercise to create quesion

--- resources/js/components/PlacementTest.vue
+++ resources/js/components/PlacementTest.vue
@@ -1,8 +1,9 @@
 <template>
     <div>
-        <section class="row home-main-section p-5 container-fluid">
+        
+        <section class="row home-main-section p-5 container-fluid"  :style="{ textAlign: currentLanguage == 'ar' ? 'right' : 'left' }">
             <div class="col-lg m-auto pl-4">
-                <p v-html="contactInfo.placement_desc"></p>
+                <p v-html="getPlacementDesc(contactInfo) "></p>
                 <router-link v-if="isLogged" :to="'/placement-questions'" class="a-link">
                     <div class="level  w-50 text-center text-light mt-1 pt-2 pb-2">
                          {{ $t('StartTest') }}
@@ -25,7 +26,7 @@
 export default {
     data() {
         return {
-
+            currentLanguage: this.$i18n.locale,
         }
 
     },
@@ -44,7 +45,17 @@ export default {
     },
 
     methods: {
+        getPlacementDesc(item) {
+        const slugs = {
+          en: item.placement_desc_en,
+          ar: item.placement_desc_ar,
+          fr: item.placement_desc_fr,
+          es: item.placement_desc_es,
+          de: item.placement_desc_de,
+        };
 
+        return slugs[this.$i18n.locale] || item.placement_desc_de;
+      }
     },
 
 

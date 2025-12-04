32e563290e0e420fa7472070efda9d48a064e137
Hamada <hamada@Hamadas-MBP.local>
2025-12-04 14:01:31 +0300

    view all exercise

--- routes/web.php
+++ routes/web.php
@@ -38,7 +38,12 @@ Route::get('/insert', function() {
 
 
 Route::get('get-collection', 'FirebaseController@hamadaindex');
-Route::get('google-login', 'GoogleLoginController@redirectToGoogle')->name('google.redirect');
+Route::get('en/google-login', 'GoogleLoginController@redirectToGoogle')->name('google.redirect');
+Route::get('ar/google-login', 'GoogleLoginController@redirectToGoogle')->name('google.redirect');
+Route::get('de/google-login', 'GoogleLoginController@redirectToGoogle')->name('google.redirect');
+Route::get('fr/google-login', 'GoogleLoginController@redirectToGoogle')->name('google.redirect');
+Route::get('es/google-login', 'GoogleLoginController@redirectToGoogle')->name('google.redirect');
+
 Route::get('/google/callback', 'GoogleLoginController@handleGoogleCallback')->name('google.callback');
 
 

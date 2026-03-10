95b407d066857a7935a4c18c75e2a0061248c05b
Hamada <hamada@Hamadas-MBP.local>
2026-03-10 19:47:36 +0300

    get content in view sourc

--- routes/web.php
+++ routes/web.php
@@ -17,107 +17,49 @@ use App\User;
 */
 
 
-Route::get('/', function () {
-    // return view('front.front');
-    // dd('xxm');
-    return view('front.front',['data' => '']);
-});
+// Route::get('/', function () {
+   
+//     return view('front.front',['data' => '']);
+// });
 
+Route::get('/', 'Admin\DashBoardController@index');
 
-Route::get('/insert', function() {
-    $students = app('firebase.firestore')->database()->collection('users')->documents();
-      // dd($students);
-    $stuRef = app('firebase.firestore')->database()->collection('users')->newDocument();
-    $stuRef->set([
-        'firstname' => 'Seven',
-        'lastname' => 'Stac',
-    ]);
-    echo "<h1>".'inserted'."</h1>";
-});
-
-
-
-Route::get('get-collection', 'FirebaseController@hamadaindex');
-Route::get('en/google-login', 'GoogleLoginController@redirectToGoogle')->name('google.redirect');
-Route::get('ar/google-login', 'GoogleLoginController@redirectToGoogle')->name('google.redirect');
-Route::get('de/google-login', 'GoogleLoginController@redirectToGoogle')->name('google.redirect');
-Route::get('fr/google-login', 'GoogleLoginController@redirectToGoogle')->name('google.redirect');
-Route::get('es/google-login', 'GoogleLoginController@redirectToGoogle')->name('google.redirect');
-
-Route::get('/google/callback', 'GoogleLoginController@handleGoogleCallback')->name('google.callback');
+// Route::get('/insert', function() {
+//     $students = app('firebase.firestore')->database()->collection('users')->documents();
+//     $stuRef = app('firebase.firestore')->database()->collection('users')->newDocument();
+//     $stuRef->set([
+//         'firstname' => 'Seven',
+//         'lastname' => 'Stac',
+//     ]);
+//     echo "<h1>".'inserted'."</h1>";
+// });
 
 
 
-Route::get('/data-t', 'DataController@process');
+// Route::get('get-collection', 'FirebaseController@hamadaindex');
+// Route::get('en/google-login', 'GoogleLoginController@redirectToGoogle')->name('google.redirect');
+// Route::get('ar/google-login', 'GoogleLoginController@redirectToGoogle')->name('google.redirect');
+// Route::get('de/google-login', 'GoogleLoginController@redirectToGoogle')->name('google.redirect');
+// Route::get('fr/google-login', 'GoogleLoginController@redirectToGoogle')->name('google.redirect');
+// Route::get('es/google-login', 'GoogleLoginController@redirectToGoogle')->name('google.redirect');
 
-
-  Route::get('/myanswer', function () {
-    // return view('front.front');
-    return view('front.myanswer');
-});
-
-// Route::any('/{any?}', 'AppController@show')->where('any', '^((?!admin|api).)*$');
-
-// Route::any('{any}', function ($any) {
-//     return view('front.front');
-// })->where('any', '^((?!admin).)*$');
-
-Route::get('{any}',function($any){
-
-      return view('front.front',['data' => '']);
-  })->where('any','^((?!admin|api|activation|myanswer).)*$');
-
-  // Route::get('{any}',function($any){
-  //     return view('front.frontt');
-  // })->where('any','.*');
-  
+// Route::get('/google/callback', 'GoogleLoginController@handleGoogleCallback')->name('google.callback');
 
 
 
-// Route::get('lang/{locale}', 'LocalizationController@index');
+// Route::get('/data-t', 'DataController@process');
+
+
+//   Route::get('/myanswer', function () {
+//     return view('front.myanswer');
+// });
+
+// Route::get('{any}',function($any){
+
+//       return view('front.front',['data' => '']);
+//   })->where('any','^((?!admin|api|activation|myanswer).)*$');
 
 
 
 
 
-
-//--start firebase-------------------
-    // Route::get('/allmyuser', function() {
-    //      $students = app('firebase.firestore')->database()->collection('User');
-    //      rint_r($students);
-    // });
-    //
-    // Route::get('/insert', function() {
-    //      $stuRef = app('firebase.firestore')->database()->collection('User')->newDocument();
-    //      $stuRef->set([
-    //         'firstname' => 'hamada',
-    //         'lastname' => 'ali',
-    //         'age'    => 19
-    //      ]);
-    //      echo "<h1>".'inserted'."</h1>";
-    // });
-    //
-    // Route::get('/display', function(){
-    //     $student = app('firebase.firestore')->database()->collection('User')->document('2b7e2c4eb51548aaa844')->snapshot();
-    //   print_r('Student ID ='.$student->id());
-    //   print_r("<br>". 'Student Name = '.$student->data()['firstname']);
-    //   print_r("<br>".'Student Age = '.$student->data()['age']);
-    // });
-    //
-    // Route::get('/update', function(){
-    //   $student = app('firebase.firestore')->database()->collection('User')->document('2b7e2c4eb51548aaa844')
-    //  ->update([
-    //   ['path' => 'age', 'value' => '18']
-    //  ]);
-    //  echo "<h1>".'updated'."</h1>";
-    // });
-    //
-    // Route::get('/delete', function(){
-    //  app('firebase.firestore')->database()->collection('User')->document('738007291ed44c49b7ca')->delete();
-    //  echo "<h1>".'deleted'."</h1>";
-    // });
-    //
-    // Route::resource('/crud', App\Http\Controllers\CrudController::class);
-    // Route::get('get-collection', [FirebaseController::class, 'hamadaindex']);
-//--end firebase-----------
-// 

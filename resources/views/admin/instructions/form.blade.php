10e859536eeb249e7fbdc20e69efea7119c39aaf
Hamada <hamada@Hamadas-MBP.local>
2026-08-21 11:21:16 +0300

    api get report

--- resources/views/admin/instructions/form.blade.php
+++ resources/views/admin/instructions/form.blade.php
@@ -18,31 +18,31 @@
 
 <div class="form-group">
     <label>German</label>
-    <textarea name="desc_de" id="ckeditor" class="form-control"
+    <textarea name="desc_de" id="ckeditor" class="form-control ckeditor"
         rows="5">{{ old('desc_de',$instruction->desc_de ?? '') }}</textarea>
 </div>
 
 <div class="form-group">
     <label>Arabic</label>
-    <textarea name="desc_ar" id="ckeditor" class="form-control"
+    <textarea name="desc_ar" id="ckeditor" class="form-control ckeditor"
         rows="5">{{ old('desc_ar',$instruction->desc_ar ?? '') }}</textarea>
 </div>
 
 <div class="form-group">
     <label>English</label>
-    <textarea name="desc_en" id="ckeditor" class="form-control"
+    <textarea name="desc_en" id="ckeditor" class="form-control ckeditor"
         rows="5">{{ old('desc_en',$instruction->desc_en ?? '') }}</textarea>
 </div>
 
 <div class="form-group">
     <label>French</label>
-    <textarea name="desc_fr" id="ckeditor" class="form-control"
+    <textarea name="desc_fr" id="ckeditor" class="form-control ckeditor"
         rows="5">{{ old('desc_fr',$instruction->desc_fr ?? '') }}</textarea>
 </div>
 
 <div class="form-group">
     <label>Spanish</label>
-    <textarea name="desc_es" id="ckeditor" class="form-control"
+    <textarea name="desc_es" id="ckeditor" class="form-control ckeditor"
         rows="5">{{ old('desc_es',$instruction->desc_es ?? '') }}</textarea>
 </div>
 
